ae7a3bf832038b19d229c9d4d8da1afa2feab01e
Bryan Libod-Libod <bryan@invedent.com>
2025-04-20 16:15:32 +0800

    setup prisma

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-npm run start:dev
+echo "Generate Prisma"
+npx prisma generate
+
+if [ $? -eq 0 ]; then
+  echo "Prisma generation successful. Starting development server..."
+  npm run start:dev
+else
+  echo "Prisma generation failed. Development server not started."
+fi
+
 exit 0
